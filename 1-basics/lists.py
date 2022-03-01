8602ebab56412502aed76869192966a0b0b159c6
Spade Black <tapendu5966@gmail.com>
2022-03-01 13:33:09 +0530

    "sets"

--- 1-basics/lists.py
+++ 1-basics/lists.py
@@ -57,3 +57,25 @@ def myfunc(n):
 thislist = [100, 50, 65, 82, 23]
 thislist.sort(key = myfunc)
 print(thislist)
+
+copy= thislist.copy()
+copy.pop()
+print(copy,thislist)
+
+"""
+# List Methods
+# Python has a set of built-in methods that you can use on lists.
+
+# Methods	    Description
+# append()	    Adds an element at the end of the list
+# clear()	    Removes all the elements from the list
+# copy()	    Returns a copy of the list
+# count()	    Returns the number of elements with the specified value
+# extend()	    Add the elements of a list (or any iterable), to the end of the current list
+# index()	    Returns the index of the first element with the specified value
+# insert()	    Adds an element at the specified position
+# pop()	        Removes the element at the specified position
+# remove()	    Removes the item with the specified value
+# reverse()	    Reverses the order of the list
+# sort()	    Sorts the list
+"""
